ac8dbecc1593f52445431d443299cb466203da8c
Hamza Younis <hamzayounis106@gmail.com>
2024-05-15 00:23:45 +0500

    Uploading project

---
+++ app.js
@@ -0,0 +1,133 @@
+// //Importing the  packages
+const axios = require("axios");
+const express = require("express");
+const fetch = require("node-fetch");
+require("dotenv").config();
+
+//Creating the server
+const app = express();
+
+//Const port
+const PORT = process.env.PORT || 3000;
+
+//Set Template Engine
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//Parse HTML data from the POST request : This line tells Express to parse incoming requests that are URL-encoded. This is necessary when you're sending data from a client-side form to the server
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+//Routing
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.post("/convert-mp3", async (req, res) => {
+  const videoURL = req.body.videoUrl;
+  const videoURLEntered = videoURL.split("v=")[1];
+  const ampersandPosition = videoURLEntered.indexOf("&");
+  if (ampersandPosition != -1) {
+    videoURLEntered = videoURLEntered.substring(0, ampersandPosition);
+    console.log(videoURLEntered);
+  }
+  if (
+    videoURLEntered === undefined ||
+    videoURLEntered === "" ||
+    videoURLEntered === null
+  ) {
+    return res.render("index", {
+      success: false,
+      message: "Please enter a URL",
+    });
+  } else {
+    try {
+      const options = {
+        method: "GET",
+        url: "https://youtube-mp36.p.rapidapi.com/dl",
+        params: { id: videoURLEntered },
+        headers: {
+          "x-rapidapi-key": process.env.API_KEY,
+          "x-rapidapi-host": process.env.API_HOST,
+        },
+      };
+
+      const response = await axios.request(options);
+      const result = response.data;
+      console.log(result);
+      if (result.status === "ok") {
+        console.log("OK");
+        return res.render("index", {
+          success: true,
+          songLink: result.link,
+          title_video: result.title,
+          fileSize: result.filesize,
+          duration: result.duration,
+        });
+      } else {
+        console.log("False");
+        return res.render("index", {
+          success: false,
+          message: result.msg,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+});
+// app.post("/convert-mp3", async (req, res) => {
+//   const videoURLEntered = req.body.videoUrl;
+//   console.log(videoURLEntered);
+//   if (
+//     videoURLEntered === undefined ||
+//     videoURLEntered === "" ||
+//     videoURLEntered === null
+//   ) {
+//     return res.render("index", {
+//       success: false,
+//       message: "Please enter a URL",
+//     });
+//   } else {
+//     try {
+//       const response = await fetch(
+//         `https://youtube-mp315.p.rapidapi.com/?url=${encodeURIComponent(
+//           videoURLEntered
+//         )}`,
+//         {
+//           method: "GET",
+//           "x-rapidapi-key":
+//             "4aa43496b4msh2bc28987d8f661ep110a22jsnbd1e68db58a5",
+//           "x-rapidapi-host": "youtube-mp315.p.rapidapi.com",
+//           "Content-Type": "application/json",
+//         }
+//       );
+//       console.log(response);
+//       const result = await response.json();
+//       console.log(result);
+//       if (result.status === "ok") {
+//         console.log("OK");
+//         return res.render("index", {
+//           success: true,
+//           songLink: result.link,
+//         });
+//       } else {
+//         console.log("False");
+//         return res.render("index", {
+//           success: false,
+//           message: result.msg,
+//         });
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+// });
+
+//Listen port
+app.listen(PORT, () => {
+  console.log(`Server is listening at the port ${PORT}`);
+});
---
+++ readme.md
@@ -0,0 +1,45 @@
+# YouTube to MP3 Converter
+
+This is a simple web application that allows users to convert YouTube videos into MP3 format. The application is built using Node.js, Express.js, and Tailwind CSS for the frontend.
+
+## Features
+
+- Convert YouTube videos to MP3
+- Display video details such as title, file size, and duration
+- User-friendly interface built with Tailwind CSS
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+- Node.js
+- npm
+
+### Installation
+
+1. Clone the repo
+   git clone https://github.com/your_username_/Project-Name.git
+
+2. Install NPM packages
+   npm install
+3. Enter your API in `.env`
+   API_KEY = 'ENTER YOUR API'; API_HOST = 'ENTER YOUR HOST';
+
+## Usage
+
+Start the server:
+node app.js
+
+Open your web browser and navigate to `http://localhost:3000` to start using the application.
+
+## Contributing
+
+Contributions are what make the open-source community such an amazing place to learn, inspire, and create. Any contributions you make are greatly appreciated.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
